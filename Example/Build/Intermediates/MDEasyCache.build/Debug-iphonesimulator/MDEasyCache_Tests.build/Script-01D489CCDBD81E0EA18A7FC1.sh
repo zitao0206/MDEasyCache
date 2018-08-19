d139f89821d75798b830221359f5d5b631e13c76
Leo <zitao.li@quvideo.com>
2018-08-19 11:05:08 +0800

    init example

---
+++ Example/Build/Intermediates/MDEasyCache.build/Debug-iphonesimulator/MDEasyCache_Tests.build/Script-01D489CCDBD81E0EA18A7FC1.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-MDEasyCache_Tests/Pods-MDEasyCache_Tests-frameworks.sh"
+
---
+++ Example/Pods/Target Support Files/MDEasyCache/MDEasyCache-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MDEasyCache : NSObject
+@end
+@implementation PodsDummy_MDEasyCache
+@end
